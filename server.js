eb4457f64d82880c0e3cfacdc2781616b98b786e
Adrian Werimo <adrwer7@gmail.com>
2022-01-19 14:15:33 +0300

    DB setup

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
 const { append } = require('express/lib/response')
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
+
+connectDB()
 
 const transactions = require('./routes/transactions')
 
